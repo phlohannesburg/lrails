60d4f90e27d5e7497ccdfe2d27267d10674dda5c
Gerry l <gerald.leblond@gmail.com>
2011-02-05 23:03:27 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lrails::Application.config.secret_token = 'b107989788d337af2710879181afb72c5d28ad9baa62397df71b20b87d893e5df9cee3c59504366c2344d4f72b0c8ddfbe626c2c5cabca7caed5c741025ed0f4'
